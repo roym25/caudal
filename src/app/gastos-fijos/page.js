fb84a045dc1dc8a9d977b6aab3efa1183ad8f540
Rodrigo Muñoz <rodrigomunozruezga@gmail.com>
2026-04-08 17:04:39 -0600

    feat: add nominas, gastos-fijos and gastos-variables pages

---
+++ src/app/gastos-fijos/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Gastos Fijos</h1>
+    </main>
+  )
+}
---
+++ src/app/gastos-variables/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Gastos Variables</h1>
+    </main>
+  )
+}
